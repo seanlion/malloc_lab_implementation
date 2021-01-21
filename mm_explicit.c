83627e3f72e6394dc21f3529834de73dbd8d9cd9
seanlion <skyhs3507@likelion.org>
2021-01-21 01:29:00 +0000

    for peer review

--- mm_explicit.c
+++ mm_explicit.c
@@ -1,19 +1,7 @@
-/* Blocks are aligned to double-word boundaries.  This
- * yields 8-byte aligned blocks on a 32-bit processor, and 16-byte aligned
- * blocks on a 64-bit processor.  However, 16-byte alignment is stricter
- * than necessary; the assignment only requires 8-byte alignment.  The
- * minimum block size taken is 4 words.
- * This allocator uses the size of a pointer, e.g., sizeof(void *), to
- * define the size of a word.  This allocator also uses the standard
- * type uintptr_t to define unsigned integers that are the same size
- * as a pointer, i.e., sizeof(uintptr_t) == sizeof(void *).
- */
-
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
- 
 #include "memlib.h"
 #include "mm.h"
 /*********************************************************
@@ -30,7 +18,7 @@ team_t team = {
 	};
 
 //* Basic constants and macros: */
-#define WSIZE sizeof(void *) /* Word and header/footer size (bytes) */
+#define WSIZE 4 /* Word and header/footer size (bytes) */
 #define DSIZE (2 * WSIZE)	 /* Doubleword size (bytes) */
 #define CHUNKSIZE (1 << 12)	 /* Extend heap by this amount (bytes) */
 
@@ -57,9 +45,6 @@ team_t team = {
 #define PREV_BLK(bp) ((void *)(bp) - GET_SIZE((void *)(bp)-DSIZE))
 
 /* Given ptr in free list, get next and previous ptr in the list */
-/* bp is address of the free block. Since minimum Block size is 16 bytes, 
-   we utilize to store the address of previous block pointer and next block pointer.
-*/
 #define GET_NEXT_PTR(bp) (*(char **)(bp + WSIZE))
 #define GET_PREV_PTR(bp) (*(char **)(bp))
 
@@ -67,11 +52,11 @@ team_t team = {
 #define SET_NEXT_PTR(bp, qp) (GET_NEXT_PTR(bp) = qp)
 #define SET_PREV_PTR(bp, qp) (GET_PREV_PTR(bp) = qp)
 
-/* Global declarations */
+/* 글로벌 변수 */
 static char *heap_listp = 0;
 static char *free_list_start = 0;
 
-/* Function prototypes for internal helper routines */
+/* Function prototypes */
 static void *coalesce(void *bp);
 static void *extend_heap(size_t words);
 static void *find_fit(size_t asize);
@@ -81,24 +66,18 @@ static void place(void *bp, size_t asize);
 static void insert_in_free_list(void *bp);
 static void remove_from_free_list(void *bp);
 
-/* Function prototypes for heap consistency checker routines: */
+/* heap consistency checker routines: 이건 직접 로직을 구현하진 않고 참고 코드 그대로. */
 static void checkblock(void *bp);
 static void checkheap(bool verbose);
 static void printblock(void *bp);
 
-/**
- * Initialize the memory manager.
- * @param - void no parameter passed in
- * @return - int 0 for success or -1 for failure
- */
 int mm_init(void)
 {
-
 	/* Create the initial empty heap. */
-	if ((heap_listp = mem_sbrk(8 * WSIZE)) == NULL) //왜 8 WSIZE???? 그냥 충분하게??
+	if ((heap_listp = mem_sbrk(8 * WSIZE)) == NULL) // 8 * WSIZE에 큰 이유는 없음.
 		return -1;
 
-	PUT(heap_listp, 0);							   /* Alignment padding */
+	PUT(heap_listp, 0);							   /* padding 만듬 */
 	PUT(heap_listp + (1 * WSIZE), PACK(DSIZE, 1)); /* Prologue header */
 	PUT(heap_listp + (2 * WSIZE), PACK(DSIZE, 1)); /* Prologue footer */
 	PUT(heap_listp + (3 * WSIZE), PACK(0, 1));	   /* Epilogue header */
@@ -111,111 +90,29 @@ int mm_init(void)
 	return 0;
 }
 
-/* 
- * Requires:
- *   size of memory asked by the programmer.
- *
- * Effects:
- *   Allocate a block with at least "size" bytes of payload, unless "size" is
- *   zero.  Returns the address of this block if the allocation was successful
- *   and NULL otherwise.
- */
-void *mm_malloc(size_t size)
+static void *extend_heap(size_t words)
 {
-	size_t asize;	   /* Adjusted block size */
-	size_t extendsize; /* Amount to extend heap if no fit */
-	void *bp;
-
-	/* Ignore spurious requests. */
-	if (size == 0)
-		return (NULL);
-
-	/* Adjust block size to include overhead and alignment reqs. */
-	if (size <= DSIZE)
-		asize = 2 * DSIZE;
-	else
-		asize = DSIZE * ((size + DSIZE + (DSIZE - 1)) / DSIZE);
-
-	/* Search the free list for a fit. */
-	if ((bp = find_fit(asize)) != NULL)
-	{
-		place(bp, asize);
-		return (bp);
-	}
-
-	/* No fit found.  Get more memory and place the block. */
-	extendsize = MAX(asize, CHUNKSIZE);
-	if ((bp = extend_heap(extendsize / WSIZE)) == NULL)
-		return (NULL);
-	place(bp, asize);
-	return (bp);
-}
-
-/* 
- * Requires:
- *   "bp" is either the address of an allocated block or NULL.
- *
- * Effects:
- *   Free a block.
- */
-void mm_free(void *bp)
-{
+	char *bp;
 	size_t size;
-	/* Ignore spurious requests. */
-	if (bp == NULL)
-		return;
-	/* Free and coalesce the block. */
-	size = GET_SIZE(HDRP(bp));
-	PUT(HDRP(bp), PACK(size, 0));
-	PUT(FTRP(bp), PACK(size, 0));
-	coalesce(bp);
-}
 
-
-void *mm_realloc(void *bp, size_t size)
-{
-	if ((int)size < 0)
-		return NULL;
-	else if ((int)size == 0)
+	/* Allocate an even number of words to maintain alignment */
+	size = (words % 2) ? (words + 1) * WSIZE : words * WSIZE;
+	// minimum block을 16사이즈로 정함
+	if (size < 16)
 	{
-		mm_free(bp);
+		size = 16;
+	}
+	/* call for more memory space */
+	if ((int)(bp = mem_sbrk(size)) == -1)
+	{
 		return NULL;
 	}
-	else if (size > 0)
-	{
-		size_t oldsize = GET_SIZE(HDRP(bp));
-		size_t newsize = size + 2 * WSIZE; // 2 words for header and footer
-		/*if newsize is less than oldsize then we just return bp */
-		if (newsize <= oldsize)
-		{
-			return bp;
-		}
-		/*if newsize is greater than oldsize */
-		else
-		{
-			size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLK(bp)));
-			size_t csize;
-			/* next block is free and the size of the two blocks is greater than or equal the new size  */
-			/* then we only need to combine both the blocks  */
-			if (!next_alloc && ((csize = oldsize + GET_SIZE(HDRP(NEXT_BLK(bp))))) >= newsize)
-			{
-				remove_from_free_list(NEXT_BLK(bp));
-				PUT(HDRP(bp), PACK(csize, 1));
-				PUT(FTRP(bp), PACK(csize, 1));
-				return bp;
-			}
-			else
-			{
-				void *new_ptr = mm_malloc(newsize);
-				place(new_ptr, newsize);
-				memcpy(new_ptr, bp, newsize);
-				mm_free(bp);
-				return new_ptr;
-			}
-		}
-	}
-	else
-		return NULL;
+	/* Initialize free block header/footer and the epilogue header */
+	PUT(HDRP(bp), PACK(size, 0));		 /* free block header */
+	PUT(FTRP(bp), PACK(size, 0));		 /* free block footer */
+	PUT(HDRP(NEXT_BLK(bp)), PACK(0, 1)); /* new epilogue header */
+	/* coalesce bp with next and previous blocks */
+	return coalesce(bp);
 }
 
 static void *coalesce(void *bp) 
@@ -253,45 +150,50 @@ static void *coalesce(void *bp)
 		bp = PREV_BLK(bp);
 		PUT(HDRP(bp), PACK(size, 0));
 		PUT(FTRP(bp), PACK(size, 0));
-	} /* lastly insert bp into free list and return bp */
+	}
 	insert_in_free_list(bp);
 	return bp;
 }
 
-static void *extend_heap(size_t words)
+void *mm_malloc(size_t size)
 {
-	char *bp;
-	size_t size;
+	size_t asize;	   /* 조정 block size */
+	size_t extendsize; /* fit 안 됐으면 늘려야 할 사이즈 */
+	void *bp;
 
-	/* Allocate an even number of words to maintain alignment */
-	size = (words % 2) ? (words + 1) * WSIZE : words * WSIZE;
-	//Since minimum block size given to us is 4 words (ie 16 bytes)
-	if (size < 16)
+	if (size == 0)
+		return (NULL);
+
+	/* Adjust block size 하는거 */
+	if (size <= DSIZE)
+		asize = 2 * DSIZE;
+	else
+		asize = DSIZE * ((size + DSIZE + (DSIZE - 1)) / DSIZE);
+
+	// explicit은 first fit으로
+	if ((bp = find_fit(asize)) != NULL)
 	{
-		size = 16;
+		place(bp, asize);
+		return (bp);
 	}
-	/* call for more memory space */
-	if ((int)(bp = mem_sbrk(size)) == -1)
-	{
-		return NULL;
-	}
-	/* Initialize free block header/footer and the epilogue header */
-	PUT(HDRP(bp), PACK(size, 0));		 /* free block header */
-	PUT(FTRP(bp), PACK(size, 0));		 /* free block footer */
-	PUT(HDRP(NEXT_BLK(bp)), PACK(0, 1)); /* new epilogue header */
-	/* coalesce bp with next and previous blocks */
-	return coalesce(bp);
+
+	/* No fit found.  Get more memory and place the block. */
+	extendsize = MAX(asize, CHUNKSIZE);
+	if ((bp = extend_heap(extendsize / WSIZE)) == NULL)
+		return (NULL);
+	place(bp, asize);
+	return (bp);
 }
-
 
 static void *find_fit(size_t asize)
 {
 	void *bp;
 	static int last_malloced_size = 0;
 	static int repeat_counter = 0;
+	// 계속 똑같은 사이즈만 요청했을 때 요청 횟수가 60회가 넘어가면 아예 힙사이즈 확늘린다.(성능을 높이기 위한 방법) 
 	if (last_malloced_size == (int)asize)
 	{
-		if (repeat_counter > 60) // 성능을 위해
+		if (repeat_counter > 60)
 		{
 			int extendsize = MAX(asize, 4 * WSIZE);
 			bp = extend_heap(extendsize / 4);
@@ -302,7 +204,7 @@ static void *find_fit(size_t asize)
 	}
 	else
 		repeat_counter = 0;
-
+	// free block list에서 first fit으로 찾기
 	for (bp = free_list_start; GET_ALLOC(HDRP(bp)) == 0; bp = GET_NEXT_PTR(bp))
 	{
 		if (asize <= (size_t)GET_SIZE(HDRP(bp)))
@@ -343,8 +245,8 @@ static void insert_in_free_list(void *bp)
 	SET_PREV_PTR(bp, NULL);
 	free_list_start = bp;
 }
-/*Removes the free block pointer int the free_list*/
 
+/*Removes the free block pointer int the free_list*/
 static void remove_from_free_list(void *bp)
 {
 
@@ -356,17 +258,69 @@ static void remove_from_free_list(void *bp)
 	SET_PREV_PTR(GET_NEXT_PTR(bp), GET_PREV_PTR(bp));
 }
 
-/* 
- * The remaining routines are heap consistency checker routines. 
- */
+void mm_free(void *bp)
+{
+	size_t size;
+	if (bp == NULL)
+		return;
+	/* Free and coalesce the block. */
+	size = GET_SIZE(HDRP(bp));
+	PUT(HDRP(bp), PACK(size, 0));
+	PUT(FTRP(bp), PACK(size, 0));
+	coalesce(bp);
+}
 
-/*
- * Requires:
- *   "bp" is the address of a block.
- *
- * Effects:
- *   Perform a minimal check on the block "bp".
- */
+
+void *mm_realloc(void *bp, size_t size)
+{
+	if ((int)size < 0)
+		return NULL;
+	else if ((int)size == 0)
+	{
+		mm_free(bp);
+		return NULL;
+	}
+	else if (size > 0)
+	{
+		size_t oldsize = GET_SIZE(HDRP(bp));
+		size_t newsize = size + (2 * WSIZE); // 2 words for header and footer
+		/*if newsize가 oldsize보다 작거나 같으면 그냥 그대로 써도 됨. just return bp */
+		if (newsize <= oldsize)
+		{
+			return bp;
+		}
+		//oldsize 보다 new size가 크면 바꿔야 함.*/
+		else
+		{
+			size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLK(bp)));
+			size_t csize;
+			/* next block is free and the size of the two blocks is greater than or equal the new size  */
+			/* next block이 가용상태이고 old, next block의 사이즈 합이 new size보다 크면 그냥 그거 바로 합쳐서 쓰기  */
+			if (!next_alloc && ((csize = oldsize + GET_SIZE(HDRP(NEXT_BLK(bp))))) >= newsize)
+			{
+				remove_from_free_list(NEXT_BLK(bp));
+				PUT(HDRP(bp), PACK(csize, 1));
+				PUT(FTRP(bp), PACK(csize, 1));
+				return bp;
+			}
+			// 아니면 새로 block 만들어서 거기로 옮기기
+			else
+			{
+				void *new_ptr = mm_malloc(newsize);
+				place(new_ptr, newsize);
+				memcpy(new_ptr, bp, newsize);
+				mm_free(bp);
+				return new_ptr;
+			}
+		}
+	}
+	else
+		return NULL;
+}
+
+
+
+// heap consistency check하기
 static void checkblock(void *bp)
 {
 
